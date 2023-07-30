840dff3611a56e56c9b2d5bd55bab945d03649b1
Evgeniy <119398191+evgenii-erokhin@users.noreply.github.com>
2023-07-30 18:51:15 +0300

    Rewrote manage-comand for import csv files.

--- backend/recipes/management/commands/import_ingredients_from_csv.py
+++ backend/recipes/management/commands/import_ingredients_from_csv.py
@@ -1,5 +1,6 @@
 import csv
 
+
 from django.core.management.base import BaseCommand
 
 from recipes.models import Ingredient
@@ -11,10 +12,9 @@ class Command(BaseCommand):
         with open('D:/Dev/foodgram-project-react/data/ingredients.csv',
                   'r',
                   encoding='utf-8') as f:
-            reader = csv.reader(f)
-            for row in reader:
-                Ingredient.objects.create(
-                    name=row[0],
-                    measurement_unit=row[1]
-                )
+            reader = csv.DictReader(f)
+            Ingredient.objects.bulk_create(
+                    Ingredient(**row) for row in reader
+            )
+
         self.stdout.write(self.style.SUCCESS('Ингредиенты успешно загруженны'))
