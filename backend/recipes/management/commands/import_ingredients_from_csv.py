fa3b38d8ea586b7f2d5e1b89ec984bced2b204b7
Evgeniy <119398191+evgenii-erokhin@users.noreply.github.com>
2023-07-30 19:01:25 +0300

    Rewrote path to ingredients.csv.

--- backend/recipes/management/commands/import_ingredients_from_csv.py
+++ backend/recipes/management/commands/import_ingredients_from_csv.py
@@ -1,6 +1,5 @@
 import csv
 
-
 from django.core.management.base import BaseCommand
 
 from recipes.models import Ingredient
@@ -9,7 +8,7 @@ from recipes.models import Ingredient
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        with open('D:/Dev/foodgram-project-react/data/ingredients.csv',
+        with open('../data/ingredients.csv',
                   'r',
                   encoding='utf-8') as f:
             reader = csv.DictReader(f)
