ab37cafad6048a6da4ed21c339e4fa58c6e19600
Evgenii Erokhin <evgeniierokhin@proton.me>
2023-08-02 16:13:17 +0300

    Edited manage command. And replace data folder tp backend

--- backend/recipes/management/commands/import_ingredients_from_csv.py
+++ backend/recipes/management/commands/import_ingredients_from_csv.py
@@ -1,5 +1,6 @@
 import csv
 
+from django.conf import settings
 from django.core.management.base import BaseCommand
 
 from recipes.models import Ingredient
@@ -8,7 +9,7 @@ from recipes.models import Ingredient
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        with open('../data/ingredients.csv',
+        with open(f'{settings.BASE_DIR}/data/ingredients.csv',
                   'r',
                   encoding='utf-8') as f:
             reader = csv.DictReader(f)
