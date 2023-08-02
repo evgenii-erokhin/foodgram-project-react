8e23c7bf27635ac0fb0061236a69fb66ee3c43d5
Evgenii Erokhin <evgeniierokhin@proton.me>
2023-08-02 10:39:10 +0300

    Fixed issue at settings.py(rewrote-getenv at allowed_hosts)

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-cg6*%6d51ef8f#4!r3*$vmsm4)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.getenv('DEBUG', 'False') == 'True')
 
-ALLOWED_HOSTS = os.get_env('ALLOWED_HOSTS', '127.0.0.1').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1').split(',')
 
 
 # Application definition
