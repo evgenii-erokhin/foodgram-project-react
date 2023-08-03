8cefb8d1be0687bb0a50ad177ba07dac6ef374bf
Evgenii Erokhin <evgeniierokhin@proton.me>
2023-08-03 21:50:04 +0300

    Fixed admin panel

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -14,9 +14,8 @@ class IngredientAdmin(admin.ModelAdmin):
 
 class IngredientRecipeInline(admin.TabularInline):
     model = IngredientRecipes
-    # extra = 0
-    # min_num = 1
-    # max_num = 1
+    extra = 0
+    max_num = 1
 
 
 class TagInline(admin.TabularInline):
