b6c429d75743bf4ec6fd342d57471c097bc574ae
Evgenii Erokhin <evgeniierokhin@proton.me>
2023-08-03 22:48:01 +0300

    extra 5

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -14,7 +14,7 @@ class IngredientAdmin(admin.ModelAdmin):
 
 class IngredientRecipeInline(admin.TabularInline):
     model = IngredientRecipes
-    extra = 0
+    extra = 5
 
 
 class TagInline(admin.TabularInline):
