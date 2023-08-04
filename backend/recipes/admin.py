65c92bac348ee2c3067fe6c01524c7b52460bafb
Evgenii Erokhin <evgeniierokhin@proton.me>
2023-08-04 22:45:43 +0300

    Added IngredientRecipeForm

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from django.forms import ValidationError
 from recipes.models import (Favorite, Ingredient, IngredientRecipes, Recipe,
                             ShoppingCart, Tag)
 from django.forms.models import BaseInlineFormSet
